2f468e0d1c9386701a91b66c4e31e08067b9713f
Michael Nnaji <mnnaji3@uwo.ca>
2021-05-24 05:16:10 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,11 @@ class RecipesController < ApplicationController
   end
 
   def create
+     recipe = Recipe.create(recipe_params)
+     redirect_to recipe_path
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name,:quantity])
   end
 end
